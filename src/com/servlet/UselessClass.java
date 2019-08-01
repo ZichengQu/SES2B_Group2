be388a5bca4f4d8bedbd7cb2e701d34ce57229cd
quzic <quzic@DESKTOP-JJBEBJJ>
2019-08-01 23:16:36 +1000

    Basic UTSHelp project

---
+++ src/com/servlet/UselessClass.java
@@ -0,0 +1,8 @@
+package com.servlet;
+/*
+ * This class is only used to preserve the package structure, 
+ * otherwise projects imported from GitHub will not have this package.
+ */
+public class UselessClass {
+
+}
